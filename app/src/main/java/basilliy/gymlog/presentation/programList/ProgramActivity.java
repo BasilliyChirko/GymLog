c69e1c6afb154c80faf86fb16bdde48a18dc472c
Basilliy <basilliyc@gmail.com>
2017-05-29 22:58:16 +0200

    add ProgramActivity

---
+++ app/src/main/java/basilliy/gymlog/presentation/programList/ProgramActivity.java
@@ -0,0 +1,15 @@
+package basilliy.gymlog.presentation.programList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import basilliy.gymlog.R;
+
+public class ProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_program);
+    }
+}
