63f015bb313c4337ade97381b2176f9dce605ea9
Basilliy <basilliyc@gmail.com>
2017-06-03 12:33:12 +0200

    create map result

--- app/src/main/java/basilliy/gymlog/presentation/graph/GraphFragment.java
+++ app/src/main/java/basilliy/gymlog/presentation/graph/GraphFragment.java
@@ -7,16 +7,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import basilliy.gymlog.R;
+import basilliy.gymlog.application.App;
+import basilliy.gymlog.domain.entity.ExerciseResult;
 import basilliy.gymlog.presentation.navigation.FragmentOnRoot;
+import basilliy.gymlog.utils.D;
 
 
 public class GraphFragment extends FragmentOnRoot {
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         initRootActivityElement();
+        Map<String, ArrayList<ExerciseResult>> mapResult = App.getExerciseResultService().getMapResult();
         return null;
     }
 
