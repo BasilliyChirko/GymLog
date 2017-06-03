c675bb6857a0877029114f2f6a3476a5a8e18943
Basilliy <basilliyc@gmail.com>
2017-06-03 19:55:37 +0200

    test data

--- app/src/main/java/basilliy/gymlog/application/LoadData.java
+++ app/src/main/java/basilliy/gymlog/application/LoadData.java
@@ -1320,8 +1320,6 @@ public class LoadData {
 
         App.getProgramService().persist(p);
 
-        App.getActiveProgramService().set(p, Calendar.getInstance().getTime(), true);
-
     }
 
     private static void loadTestExerciseResult(){
@@ -1329,7 +1327,7 @@ public class LoadData {
         ExerciseResultService service = App.getExerciseResultService();
 
         long[] values = new long[] {
-          25, 27, 30, 35, 35, 35, 20, 24, 25, 30, 32, 25, 27, 30, 35, 35, 35, 20, 24, 25, 30, 32, 42
+          25, 25, 27, 27, 27, 27, 28, 30, 30, 31, 31, 31, 28, 25, 27, 27, 27, 28, 28, 30, 30, 30, 30, 33, 35, 35, 35, 37
         };
 
         Calendar calendar = Calendar.getInstance();
@@ -1341,7 +1339,7 @@ public class LoadData {
         }
 
         values = new long[] {
-                2, 3, 5, 1, 4
+                5, 5, 7, 7, 7, 7, 8, 10, 10, 11, 11, 11, 8, 5, 7, 7, 7, 8, 8, 10, 10, 10, 10, 13, 15, 15, 15, 17
         };
 
         calendar = Calendar.getInstance();
